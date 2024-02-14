a4856d1e0f4bae6d3354708d91ec0a7248797c80
Jihoon Jeong <jihoonjeong@hanyang.ac.kr>
2024-02-15 01:02:33 +0900

    [Gold V] Title: 평범한 배낭, Time: 272 ms, Memory: 97112 KB -BaekjoonHub

--- 백준/Gold V/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold V/12865. 평범한 배낭/평범한 배낭.java
@@ -12,11 +12,11 @@ public class Main {
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static int solution(int num, int weight) throws IOException {
-        if(visited[num][weight] == 1){
+        if(visited[num][weight] == 1){ // 중복 연산 방지
             return DP[num][weight];
         }
 
-        if(num == 1){
+        if(num == 1){ // base condition : 첫 번째 물건 도달 시 무게 조건에 따라 담을 수 있으면 담고, 아니면 0
             if(W[num] <= weight) {
                 return V[num];
             } else {
@@ -24,8 +24,10 @@ public class Main {
             }
         }
 
+        // 결국 num번째의 물건을 담느냐, 안 담느냐의 차이
+        // DP[num][weight] : 가방 속 num개의 물건들의 무게의 합이 weight 일 때의 최대 value
         visited[num][weight] = 1;
-        if(weight - W[num] >= 0){
+        if(weight - W[num] >= 0){ // num번째 물건을 담아도 무게 제한이 초과되지 않는 경우
             DP[num][weight] = Math.max(solution(num - 1, weight), solution(num - 1, weight - W[num]) + V[num]);
         } else {
             DP[num][weight] = solution(num - 1, weight);
