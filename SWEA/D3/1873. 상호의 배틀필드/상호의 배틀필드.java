aded60deb6928eb5c7e931c2b3afee90dd23779e
Jihoon Jeong <jihoonjeong@hanyang.ac.kr>
2024-02-15 14:34:19 +0900

    [D3] Title: 상호의 배틀필드, Time: 132 ms, Memory: 21,480 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -4,20 +4,20 @@ import java.util.*;
 public class Solution {
 
     static int testCase;
-    static int H, W, N;
+    static int H, W, N; // 필드 높이, 필드 너비, 명령 길이
 
-    static char [][] board;
-    static char [] cmd;
-    static int [] dr = {-1, 1, 0, 0};
-    static int [] dc = {0, 0, -1, 1};
+    static char [][] board; // 필드 상태 저장 배열
+    static char [] cmd;     // 명령 저장 배열
+    static int [] dr = {-1, 1, 0, 0}; // 행 delta
+    static int [] dc = {0, 0, -1, 1}; // 열 delta
 
-    static int tr, tc;
+    static int tr, tc; // 탱크의 현재 위치 행, 열 좌표
 
     static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static int getDirection(char d){
+    public static int getDirection(char d){ // 탱크의 방향 문자열 -> 탱크의 방향 인덱스 변환
         switch (d) {
             case '^' :
                 return 0;
@@ -31,7 +31,7 @@ public class Solution {
         return -1;
     }
 
-    public static char getDirection(int d){
+    public static char getDirection(int d){ // 탱크의 방향 인덱스 -> 탱크의 방향 문자열 변환
         switch (d) {
             case 0 :
                 return '^';
@@ -45,50 +45,50 @@ public class Solution {
         return ' ';
     }
 
-    public static void move(int nd){
-        board[tr][tc] = getDirection(nd);
+    public static void move(int nd){ // 탱크 이동 함수
+        board[tr][tc] = getDirection(nd); // 주어진 방향 nd로 탱크의 방향을 수정
 
-        int nr = tr + dr[nd];
-        int nc = tc + dc[nd];
+        int nr = tr + dr[nd]; // 이동할 행 좌표
+        int nc = tc + dc[nd]; // 이동할 열 좌표
 
-        if(nr < 0 || H <= nr || nc < 0 || W <= nc || board[nr][nc] != '.'){
+        if(nr < 0 || H <= nr || nc < 0 || W <= nc || board[nr][nc] != '.'){ // 이동할 좌표가 유효 범위 내에 없거나, 이동이 불가능한 칸인지 판단
             return;
         }
 
-        char tank = board[tr][tc];
-        board[tr][tc] = '.';
-        tr = nr;
-        tc = nc;
-        board[tr][tc] = tank;
+        char tank = board[tr][tc]; // 탱크의 위치 swap을 위해 탱크의 상태 임시 저장
+        board[tr][tc] = '.'; // 탱크 이동 후 원래 위치 평지화
+        tr = nr; // 현재 탱크 행 좌표 수정
+        tc = nc; // 현재 탱크 열 좌표 수정
+        board[tr][tc] = tank; // 탱크 이동 후 위치에 탱크 상태 저장
     }
 
-    public static boolean hit(int r, int c){
-        char element = board[r][c];
+    public static boolean hit(int r, int c){ // r, c 위치에 포탄이 도달한 경우를 처리
+        char element = board[r][c]; // r, c 좌표에 있는 요소 저장
 
-        if(element == '*'){ // 벽돌 벽 -> 파괴 후 소멸
-            board[r][c] = '.';
-            return true;
-        } else if (element == '#'){ // 강철 벽 -> 소멸
-            return true;
+        if(element == '*'){ // 벽돌 벽이 있는 경우 -> 파괴 후 소멸
+            board[r][c] = '.'; // 벽돌 파괴 후 평지화
+            return true; // 포탄 소멸
+        } else if (element == '#'){ // 강철 벽이 있는 경우 -> 소멸
+            return true; // 포탄 소멸
         } else {
-            return false;
+            return false; // 포탄 소멸되지 않음
         }
     }
 
-    public static void fire(){
-        int direction = getDirection(board[tr][tc]);
+    public static void fire(){ // 탱크의 포탄 발사 과정을 처리
+        int direction = getDirection(board[tr][tc]); // 탱크의 방향 인덱스
 
-        if(direction < 2){ // U, D
+        if(direction < 2){ // 현재 방향이 U 또는 D인 경우 -> 행 단위 발사
             for(int i = 1; i <= H; i++){
-                int nr = tr + dr[direction] * i;
-                if(nr < 0 || H <= nr || hit(nr, tc)){
+                int nr = tr + dr[direction] * i; // 발사된 포탄의 새로운 행 좌표
+                if(nr < 0 || H <= nr || hit(nr, tc)){ // 포탄이 유효 범위를 벗어나거나, 소멸하면 중지
                     break;
                 }
             }
-        } else { // L, R
+        } else { // 현재 방향이 L 또는 R인 경우 -> 열 단위 발사
             for(int i = 1; i <= W; i++){
-                int nc = tc + dc[direction] * i;
-                if(nc < 0 || W <= nc || hit(tr, nc)){
+                int nc = tc + dc[direction] * i; // 발사된 포탄의 새로운 열 좌표
+                if(nc < 0 || W <= nc || hit(tr, nc)){ // 포탄이 유효 범위를 벗어나거나, 소멸하면 중지
                     break;
                 }
             }
@@ -97,7 +97,7 @@ public class Solution {
 
     public static void solution(int tc) throws IOException {
         for(int i = 0; i < N; i++){
-            switch (cmd[i]) {
+            switch (cmd[i]) { // 각 명령에 대한 동작 수행
                 case 'U' :
                     move(0);
                     break;
@@ -116,6 +116,7 @@ public class Solution {
             }
         }
 
+        // 결과 버퍼 입력
         bw.write(String.format("#%d ", tc));
         for(int i = 0; i < H; i++){
             bw.write(board[i]);
@@ -128,28 +129,28 @@ public class Solution {
 
         for (int t = 1; t <= testCase; t++) {
             st = new StringTokenizer(br.readLine());
-            H = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken()); // 필드 높이 입력
+            W = Integer.parseInt(st.nextToken()); // 필드 너비 입력
 
             board = new char [H][W];
             for(int i = 0; i < H; i++){
-                char [] inp = br.readLine().toCharArray();
+                char [] inp = br.readLine().toCharArray(); // 필드 요소 입력
                 for(int j = 0; j < W; j++){
-                    board[i][j] = inp[j];
+                    board[i][j] = inp[j]; // 필드 배열에 각 요소 저장
 
-                    if(inp[j] == '<' || inp[j] == '>' || inp[j] == '^' || inp[j] == 'v'){
-                        tr = i;
-                        tc = j;
+                    if(inp[j] == '<' || inp[j] == '>' || inp[j] == '^' || inp[j] == 'v'){ // 입력 요소가 탱크인 경우
+                        tr = i; // 현재 탱크의 행 좌표 저장
+                        tc = j; // 현재 탱크의 열 좌표 저장
                     }
                 }
             }
 
-            N = Integer.parseInt(br.readLine());
-            cmd = br.readLine().toCharArray();
+            N = Integer.parseInt(br.readLine()); // 명령어 길이 입력
+            cmd = br.readLine().toCharArray();   // 명령어 입력
 
             solution(t);
         }
 
-        bw.close();
+        bw.close(); // 버퍼 출력
     }
 }
