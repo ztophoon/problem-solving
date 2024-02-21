2d343b3c56b7023bdb4b35d5219b0c11596ec3d9
Jihoon Jeong <jihoonjeong@hanyang.ac.kr>
2024-02-21 23:31:11 +0900

    [Gold V] Title: 강의실 배정, Time: 612 ms, Memory: 74496 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,6 +1,16 @@
 import java.io.*;
 import java.util.*;
 
+/*
+ * 강의의 시작 시간, 종료 시간을 각각 minHeap으로 정렬
+ * 오름차순으로 정렬된 시작 시간들을 차례로 순회하면서, 종료 시간들의 최소 값과 비교
+ *
+ * 1. 조회된 시작 시간이 조회 시점에서 종료 시간들의 최소값 보다 크다면, 그 최소값 뒤에 바로 이에 붙여서 강의실을 연속으로 사용 가능
+ * 2. 조회된 시작 시간이 조회 시점에서 종료 시간들의 최소값 보다 작다면, 강의를 뒤에 이어 붙일 수 있는 강의실이 없다는 것이므로 강의실 개수 추가
+ *
+ * 시작 시간과 종료 시간을 모두 오름차순으로 정렬해두었으므로 조회 시점 당시 만을 고려하여 가능한 경우의 수를 만들어간다 -> greedy
+ */
+
 public class Main {
 
     public static StringTokenizer st;
@@ -9,36 +19,18 @@ public class Main {
 
     public static int N, answer;
 
-    public static PriorityQueue<int []> timesPQ = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] < o2[0]) {
-                    return -1;
-                }
-                if (o1[0] > o2[0]) {
-                    return 1;
-                }
-                if (o1[1] < o2[1]) {
-                    return -1;
-                }
-                return 1;
-            }
-        }
-    );
-    public static PriorityQueue<Integer> endTimesPQ = new PriorityQueue<>();
+    public static PriorityQueue<Integer> startTimesPQ = new PriorityQueue<>(); // 강의 시작 시간 minHeap
+    public static PriorityQueue<Integer> termiTimesPQ = new PriorityQueue<>(); // 강의 종료 시간 minHeap
 
     public static void solution() {
-        while(!timesPQ.isEmpty()){
-            int [] sAndT = timesPQ.poll();
-            int start = sAndT[0];
-            int termi = sAndT[1];
+        while(!startTimesPQ.isEmpty()){      // 오름차순으로 정렬된 heap을 순회하면서,
+            int start = startTimesPQ.poll(); // 강의 시작 시간 조회
 
-            if(!endTimesPQ.isEmpty() && endTimesPQ.peek() <= start){
-                endTimesPQ.poll();
-            } else {
-                answer++;
+            if(!termiTimesPQ.isEmpty() && termiTimesPQ.peek() <= start){ // 조회된 시작 시간이 종료 시간들 중 최소값보다 작다면, 강의실을 연속으로 사용 가능!
+                termiTimesPQ.poll();                                     // 이때 종료 시간은 더 이상 최소 종료 시간이 아니므로 heap에서 삭제
+            } else {                                                     // 시작 시간이 종료 시간 중 최소값 보다 크다면, 반드시 새로운 강의실이 필요
+                answer++;                                                // 강의실 개수 추가
             }
-            // endTimesPQ.add(termi);
         }
     }
 
@@ -50,8 +42,8 @@ public class Main {
             int s = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
 
-            timesPQ.offer(new int [] {s, t});
-            endTimesPQ.offer(t);
+            startTimesPQ.offer(s);
+            termiTimesPQ.offer(t);
         }
 
         solution();
